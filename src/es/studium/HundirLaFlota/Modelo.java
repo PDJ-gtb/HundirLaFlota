1a85c75cef2c98ecdca8bc879b4d7d576360128c
PDJ-gtb <pacodejesus99@gmail.com>
2025-06-12 13:10:32 +0200

    Modelo

--- src/es/studium/HundirLaFlota/Modelo.java
+++ src/es/studium/HundirLaFlota/Modelo.java
@@ -1,27 +1,70 @@
 package es.studium.HundirLaFlota;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Modelo
 {
-	private int progreso;
+	String driver = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/practicaequipos";
+	String login = "admin1";
+	String password = "Studium2025#";
+	String sentencia = "";
+	Connection connection = null;
+	Statement statement = null;
+	ResultSet rs = null;
 
-    public Modelo() {
-        this.progreso = 0;
-    }
+	public Connection conectar() {
+		try
+		{
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, login, password);
 
-    public int getProgreso() {
-        return progreso;
-    }
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return connection;
 
-    public void incrementarProgreso(int cantidad) {
-        progreso += cantidad;
-        if (progreso > 100) {
-            progreso = 100;
-        }
-    }
+	}
+	public void desconectar (Connection connection) {
+		if (connection!=null) {
+			try
+			{
+				connection.close();
+			} catch (SQLException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public boolean comprobrarCredenciales(Connection conexion, String usuario, String clave) {
 
-    public boolean estaCompleto() {
-        return progreso >= 100;
-    }
+		boolean credencialesCorrectas= false;
+		sentencia = "SELECT * FROM usuarios WHERE nombreJugador= '"+usuario + "' AND passJugador = SHA2('"+ clave + "',256);";
+
+		try
+		{
+			statement = conexion.createStatement();
+			rs = statement.executeQuery(sentencia);
+
+			if (rs.next()) {
+				credencialesCorrectas=true;
+			}
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return credencialesCorrectas;
+	}
 }
 
 
